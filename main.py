6423244281eb89c8668e87b108fde8a43bbbb821
hoainam1401 <ngohoainam1401@gmail.com>
2025-02-20 21:16:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import sys
 
 import pygame
+
 from checkers.constants import GREEN, HEIGHT, SQUARE_SIZE, WIDTH
 from checkers.game import Game
 from minimax.algorithm import minimax
 
 FPS = 60
+
+# Initialize Pygame and the font module
+pygame.init()
+pygame.font.init()
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Checkers")
@@ -16,6 +21,27 @@ def get_row_col_from_mouse(pos):
     row = y // SQUARE_SIZE
     col = x // SQUARE_SIZE
     return row, col
+
+
+def draw_winner(text):
+    font = pygame.font.SysFont("MonoLisaPlus", 100)
+    text = font.render(text, 1, (255, 255, 0))
+    pygame.draw.rect(
+        WIN,
+        (255, 0, 0),
+        (
+            WIDTH // 2 - text.get_width() // 2 - 10,
+            HEIGHT // 2 - text.get_height() // 2 - 10,
+            text.get_width() + 20,
+            text.get_height() + 20,
+        ),
+        5,
+    )
+    WIN.blit(
+        text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2)
+    )
+    pygame.display.update()
+    pygame.time.delay(3000)
 
 
 def main(mode, depth):
@@ -30,8 +56,12 @@ def main(mode, depth):
             value, new_board = minimax(game.get_board(), depth, GREEN, game)
             game.ai_move(new_board)
 
-        if game.winner() is not None:
-            print("GREEN WINS!" if game.winner() == GREEN else "PURPLE WINS!")
+        winner = game.winner()
+        if winner is not None:
+            if winner == GREEN:
+                draw_winner("GREEN WINS!")
+            else:
+                draw_winner("PURPLE WINS!")
             run = False
 
         for event in pygame.event.get():
@@ -50,14 +80,9 @@ def main(mode, depth):
 if __name__ == "__main__":
     if len(sys.argv) < 2 or sys.argv[1] not in ["pvp", "cpu"]:
         print("Usage: main.py [pvp|cpu] [depth]")
-    elif sys.argv[1] == "cpu" and len(sys.argv) == 2:
-        mode = sys.argv[1]
-        depth = 1  # Default depth is 1 if not provided
-        main(mode, depth)
-    elif sys.argv[1] == "cpu" and len(sys.argv) == 3:
-        mode = sys.argv[1]
-        depth = int(sys.argv[2])
-        main(mode, depth)
     else:
         mode = sys.argv[1]
-        main(mode, 1)  # Default depth is 1 if not provided
+        depth = (
+            int(sys.argv[2]) if len(sys.argv) == 3 else 1
+        )  # Default depth is 1 if not provided
+        main(mode, depth)
